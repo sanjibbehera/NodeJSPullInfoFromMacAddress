eca182a2d4fad591e5fca4a459aca4ed6f7ec911
sanjibbehera <51856169+sanjibbehera@users.noreply.github.com>
2020-04-16 16:14:35 +0530

    2 cmdline params, macaddr and api_token to pass

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-echo "$1"
+echo "$1" "$2"
 cd /app
 npm install
 cd /app
-node server.js $1
+node server.js $1 $2
--- server.js
+++ server.js
@@ -1,22 +1,23 @@
 const https = require('https');
 
-var macaddrpath = '/v1?apiKey=at_D7J4WtYNagBtjGcaOtaW33nzeYp5x&output=json&search=';
-
 var length = process.argv.length;
-if(length != 3){
-    console.log('Invalid argument provided, pass MAC address as parameter to the program!!');
-    console.log('Proper Example is: node server.js <MAC ADDRESS>');
+if(length != 4){
+    console.log('Invalid argument provided, pass MAC address & the API_TOKEN as parameter to the program!!');
+    console.log('Proper Example is: node server.js <MAC ADDRESS> <API_TOKEN>');
     process.exit(1);
 }
 
 var myArgs = process.argv.slice(2);
 macaddrinfo = myArgs[0];
+api_token = myArgs[1];
 var regexp = /^([0-9A-Fa-f]{2}[:-]){5}([0-9A-Fa-f]{2})$/;
 var bool = regexp.test(macaddrinfo);
 if(!bool){
     console.log('Invalid MAC Address provided to the program!!');
     process.exit(1);
 }
+var macaddrpath = '/v1?apiKey=' + api_token + '&output=json&search=';
+console.log(macaddrpath);
 macaddrpath += macaddrinfo;
 
 const options = {
@@ -33,7 +34,6 @@ var req = https.request(options, function(res) {
          var CompanyName = jsonObject.vendorDetails.companyName;
          var CompanyAddress = jsonObject.vendorDetails.companyAddress;
          console.log('Vendor Details of the provided MAC Address <' + macaddrinfo +  '> is: '  + CompanyName + ' having Address: ' + CompanyAddress);
-         //console.log(jsonObject.vendorDetails);
     });
   });
   req.on('error', function(e) {
